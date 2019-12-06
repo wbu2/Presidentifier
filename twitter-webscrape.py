3ce1cc19c223cec55f6b7cd223a3e46e0e862cae
Phil Vukovic <philvukovic@gmail.com>
2019-12-05 21:47:05 -0600

    fix bugs, obama/trump data

--- twitter-webscrape.py
+++ twitter-webscrape.py
@@ -15,36 +15,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 
-handle = "@BarackObama"
-ctr = 1000
+def write_user(handle, writer):
+    driver = webdriver.Chrome()
+    driver.base_url = "https://twitter.com/" + handle
+    driver.get(driver.base_url)
 
-driver = webdriver.Chrome()
-driver.base_url = "https://twitter.com/" + handle
-driver.get(driver.base_url)
+    for i in range(1,5):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        html_source = driver.page_source
+        sourcedata = html_source.encode('utf-8')
+        bs = BeautifulSoup(sourcedata, 'lxml')
+        all_tweets = bs.find_all('div', {'class':'tweet'})
 
-for i in range(1,10):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+        if all_tweets:
+            for tweet in all_tweets:
+                content = tweet.find('div',{'class':'content'})
+                header = content.find('div',{'class':'stream-item-header'})
+                user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()
+                message_raw = content.find('div',{'class':'js-tweet-text-container'}).text.replace("\n"," ").strip()
+                message = re.sub(r'https?:\/\/.*[\r\n]*', '', message_raw, flags=re.MULTILINE)
 
-html_source = driver.page_source
-sourcedata = html_source.encode('utf-8')
-bs = BeautifulSoup(sourcedata, 'lxml')
-all_tweets = bs.find_all('div', {'class':'tweet'})
+                writer.writerow({'author': handle, 'message': message})
 
-if all_tweets:
-    with open('tweets.csv', 'w', newline='') as csvfile:
-        fields = ['author', 'message']
-        writer = csv.DictWriter(csvfile, fields)
-        writer.writeheader()
-        first_id = all_tweets[0]['data-item-id']
-        last_id = all_tweets[len(all_tweets)-1]['data-item-id']
-        
-        for tweet in all_tweets[:ctr]:
-            content = tweet.find('div',{'class':'content'})
-            header = content.find('div',{'class':'stream-item-header'})
-            user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()
-            message_raw = content.find('div',{'class':'js-tweet-text-container'}).text.replace("\n"," ").strip()
-            message = re.sub(r'https?:\/\/.*[\r\n]*', '', message_raw, flags=re.MULTILINE)
 
-            writer.writerow({'author': handle, 'message': message})
+csvfile = open('tweets.csv', 'w', newline='', encoding='utf-8')
+fields = ['author', 'message']
+writer = csv.DictWriter(csvfile, fields)
+writer.writeheader()
+write_user('@BarackObama', writer)
+write_user('@realDonaldTrump', writer)
 
