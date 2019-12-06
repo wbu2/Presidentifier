e7b99cb3310d418e03d5720782b303bf70dee54d
Phil Vukovic <philvukovic@gmail.com>
2019-12-05 20:58:18 -0600

    added infinite scroll functionality

--- twitter-webscrape.py
+++ twitter-webscrape.py
@@ -3,13 +3,32 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-import re
+import re, time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 handle = "@BarackObama"
 ctr = 1000
 
-res = requests.get('https://twitter.com/' + handle)
-bs = BeautifulSoup(res.content, 'lxml')
+driver = webdriver.Chrome()
+driver.base_url = "https://twitter.com/" + handle
+driver.get(driver.base_url)
+
+for i in range(1,10):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+html_source = driver.page_source
+sourcedata = html_source.encode('utf-8')
+bs = BeautifulSoup(sourcedata, 'lxml')
 all_tweets = bs.find_all('div', {'class':'tweet'})
 
 if all_tweets:
@@ -17,12 +36,15 @@ if all_tweets:
         fields = ['author', 'message']
         writer = csv.DictWriter(csvfile, fields)
         writer.writeheader()
+        first_id = all_tweets[0]['data-item-id']
+        last_id = all_tweets[len(all_tweets)-1]['data-item-id']
         
-        for tweet in all_tweets[:10]:
+        for tweet in all_tweets[:ctr]:
             content = tweet.find('div',{'class':'content'})
             header = content.find('div',{'class':'stream-item-header'})
             user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()
             message_raw = content.find('div',{'class':'js-tweet-text-container'}).text.replace("\n"," ").strip()
             message = re.sub(r'https?:\/\/.*[\r\n]*', '', message_raw, flags=re.MULTILINE)
-            
+
             writer.writerow({'author': handle, 'message': message})
+
