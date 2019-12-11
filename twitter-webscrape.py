29c2f3691e86a912c6c6950a90cca85659fa28fa
Phil Vukovic <philvukovic@gmail.com>
2019-12-10 19:25:38 -0600

    add 2020 democratic candidates to dataset

--- twitter-webscrape.py
+++ twitter-webscrape.py
@@ -1,4 +1,6 @@
 # https://www.w3resource.com/python-exercises/web-scraping/web-scraping-exercise-20.php
+# Philip Vukovic
+# December 10 2019
 
 from bs4 import BeautifulSoup
 import requests
@@ -19,8 +21,9 @@ def write_user(handle, writer):
     driver = webdriver.Chrome()
     driver.base_url = "https://twitter.com/" + handle
     driver.get(driver.base_url)
+    tweet_count = 0
 
-    for i in range(1,5):
+    while tweet_count < 300:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         html_source = driver.page_source
         sourcedata = html_source.encode('utf-8')
@@ -28,6 +31,7 @@ def write_user(handle, writer):
         all_tweets = bs.find_all('div', {'class':'tweet'})
 
         if all_tweets:
+            tweet_count += len(all_tweets)
             for tweet in all_tweets:
                 content = tweet.find('div',{'class':'content'})
                 header = content.find('div',{'class':'stream-item-header'})
@@ -42,6 +46,13 @@ csvfile = open('tweets.csv', 'w', newline='', encoding='utf-8')
 fields = ['author', 'message']
 writer = csv.DictWriter(csvfile, fields)
 writer.writeheader()
-write_user('@BarackObama', writer)
+write_user('@JoeBiden', writer)
+write_user('@ewarren', writer)
+write_user('@BernieSanders', writer)
+write_user('@PeteButtigieg', writer)
+write_user('@AndrewYang', writer)
+write_user('@amyklobuchar', writer)
+write_user('@TomSteyer', writer)
+write_user('@MikeBloomberg', writer)
 write_user('@realDonaldTrump', writer)
 
