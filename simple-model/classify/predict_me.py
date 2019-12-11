ffdf882dc7354c267a90ec279a61a9b783a6e248
ahsang2 <ahsang2@illinois.edu>
2019-12-10 22:38:15 -0600

    python script for model called predict_me

---
+++ simple-model/classify/predict_me.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from match_my_voice import Model
+
+
+# In[2]:
+
+
+us = Model()
+
+
+# In[3]:
+
+
+input_str = 'Thank you to Great Republican @SenJohnKennedy for the job he did in representing both the Republican Party and myself against Sleepy Eyes Chuck Todd on Meet the Depressed!'
+
+
+# In[4]:
+
+
+us.predict(input_str)
+
